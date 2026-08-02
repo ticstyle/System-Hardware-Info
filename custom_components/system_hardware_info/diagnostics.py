61f7314be692a057881de827e5de2d3de13cdf4f
dualityps <4126225+ticstyle@users.noreply.github.com>
2026-08-02 09:14:14 +0200

    Refactor diagnostics.py to include additional CPU info

--- custom_components/system_hardware_info/diagnostics.py
+++ custom_components/system_hardware_info/diagnostics.py
@@ -12,8 +12,10 @@ from homeassistant.core import HomeAssistant
 from . import SystemHardwareConfigEntry
 from .const import SYSFS_PATHS
 from .sensor import (
+    _get_bios_date,
     _get_boot_disk_model,
-    _get_cpu_model,
+    _get_cpu_info,
+    _get_cpu_max_freq,
     _get_hypervisor,
     _get_primary_mac,
     _get_total_ram,
@@ -29,9 +31,19 @@ async def async_get_config_entry_diagnostics(
     hardware_data: dict[str, str | None] = {}
 
     for key, path in SYSFS_PATHS.items():
-        hardware_data[key] = await hass.async_add_executor_job(_read_sysfs_file, path)
+        hardware_data[key] = await hass.async_add_executor_job(
+            _read_sysfs_file, path
+        )
 
-    hardware_data["cpu_model"] = await hass.async_add_executor_job(_get_cpu_model)
+    cpu_model, cpu_vendor, virt_capable = await hass.async_add_executor_job(
+        _get_cpu_info
+    )
+    hardware_data["cpu_model"] = cpu_model
+    hardware_data["cpu_vendor"] = cpu_vendor
+    hardware_data["hardware_virt"] = virt_capable
+    hardware_data["bios_date"] = await hass.async_add_executor_job(_get_bios_date)
+    hardware_data["cpu_max_freq"] = await hass.async_add_executor_job(_get_cpu_max_freq)
+    hardware_data["kernel_version"] = platform.release()
     hardware_data["cpu_cores"] = str(os.cpu_count())
     hardware_data["cpu_arch"] = platform.machine()
     hardware_data["total_ram"] = await hass.async_add_executor_job(_get_total_ram)
